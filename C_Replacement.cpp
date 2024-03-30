4b552927229081334e984640737e5176cd6ef62f
Anik Sen <cse2023anik@gmail.com>
2024-03-31 02:56:57 +0600

    C_Replacement.cpp

---
+++ C_Replacement.cpp
@@ -0,0 +1,24 @@
+//Using "transform"-->algorithm
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int main(){
+    int n;
+	cin>>n;
+	int a[n];
+	for(int i=0; i<n; i++){
+		cin>>a[i];
+	}
+    transform(a, a+n, a, [](int x){
+        if(x>0)
+            return 1;
+        else if(x<0)
+            return 2;
+        else
+            return 0;//for elements equal to zero
+    });
+    for(int i=0; i<n; i++){
+        cout<<a[i]<<' ';
+    }
+	return 0;
+}
